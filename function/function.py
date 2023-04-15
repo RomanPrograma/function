f2f418b8229de81db2174ac1f72bd755ab1eb736
Roman <roma2000kuba@gmail.com>
2023-04-16 00:21:50 +0300

    ex_7 DONE

--- function/function.py
+++ function/function.py
@@ -1,1 +1,10 @@
-
+try:
+    def function():
+        a = int(input('Enter a number: '))
+        for i in range(1, a ):
+            if a % i == 0:
+                print(i,)
+        return a
+    print(function())
+except Exception as e:
+    print(f'error: {e}')
