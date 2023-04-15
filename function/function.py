0514d40448a0a963f122d9e546a56eff441e6688
Roman <roma2000kuba@gmail.com>
2023-04-15 23:02:09 +0300

    ex_5 done

--- function/function.py
+++ function/function.py
@@ -1,1 +1,12 @@
 
+
+try:
+    def cap_words():
+        text = input('Enter a string: ').split()
+        for word in text:
+            if word.isalpha():
+                print(word.capitalize(), end=' ' )
+                
+    print(cap_words())
+except Exception as e:
+    print(f'error: {e}')
