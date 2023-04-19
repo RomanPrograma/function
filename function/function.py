502e879a59a5735eabbe9b81bb63a4f1ba045a4d
Roman <roma2000kuba@gmail.com>
2023-04-19 22:14:36 +0300

    ex_9 done

--- function/function.py
+++ function/function.py
@@ -1,1 +1,12 @@
+# -*- coding: cp1251 -*-
+try:
+    def string(list):
+        if len(list)<6:
+            return "Недостатньо символів"
+        else:
+            return list[:3] + list[-3:]
+    print(string('Добрий вечір!'))
+except Exception as e:
+    print(f'error: {e}')
+
 
