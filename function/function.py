b9846632c7be4232494b4d0ff6801a80ba494540
Roman <roma2000kuba@gmail.com>
2023-04-23 00:58:52 +0300

    ex_4 done

--- function/function.py
+++ function/function.py
@@ -1,1 +1,9 @@
-
+def fibonachi(f):
+    if f == 0:
+        return 0
+    elif f == 1:
+        return 1
+    else:
+        return fibonachi(f-1) + fibonachi(f-2)  
+f = int(input('enter '))
+print(fibonachi(f))
