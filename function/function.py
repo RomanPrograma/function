3686b0c2a367694ac90b8421aae7406f54a6a01e
Roman <roma2000kuba@gmail.com>
2023-04-23 00:45:43 +0300

    ex_7 done

--- function/function.py
+++ function/function.py
@@ -3,7 +3,7 @@ try:
         a = int(input('Enter a number: '))
         for i in range(1, a ):
             if a % i == 0:
-                print(i,)
+                print(i)
         return a
     print(function())
 except Exception as e:
