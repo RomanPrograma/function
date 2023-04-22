e1ceb394774a06b70e3cceedce51c7981a652eb0
Roman <roma2000kuba@gmail.com>
2023-04-23 01:00:14 +0300

    ex_8 done

--- function/function.py
+++ function/function.py
@@ -1,1 +1,12 @@
+n = input('Enter name: ')
+a = int(input('Enter age: '))
+n1 = input('Enter name: ')
+a1 = int(input('Enter age: '))
+n2 = input('Enter name: ')
+a2 = int(input('Enter age: '))
+list = [(n,a),(n1,a1),(n2,a2)]
+def sort_age(list):
+    list.sort(key=lambda x: x[1])
+    return(list)
+print(sort_age(list))
 
