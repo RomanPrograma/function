f8e9aae7c252173f8325636a0830a63e3e6fd3ee
Roman <roma2000kuba@gmail.com>
2023-04-23 00:53:46 +0300

    ex_3 done

--- function/function.py
+++ function/function.py
@@ -1,1 +1,13 @@
-
+import random
+list = []
+nums = int(input('length:'))
+for i in range(nums):
+    list.append(random.randint(0, 100))
+def get_unique_number(list):
+    unique = []
+    for i in list:
+        if i not in unique:
+            unique.append(i)
+    return unique
+print(list)
+print(get_unique_number(list))
