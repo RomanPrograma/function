1c037a417ed67685be3e16269e963089e05917d1
Roman <roma2000kuba@gmail.com>
2023-04-23 00:59:44 +0300

    ex_6 done

--- function/function.py
+++ function/function.py
@@ -1,1 +1,9 @@
+import random
+list=[random.randint(0, 20) for i in range(10)]
+list2=[random.randint(0, 20) for i in range(10)]
+print(list,list2)
 
+def scalar(list,list2):
+    scalar = sum([list[i]*list2[i] for i in range(len(list))])
+    return(scalar)
+print(scalar(list,list2))
