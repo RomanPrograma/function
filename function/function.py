833b42d2465be88b2072217cbfc23dc327610729
Roman <roma2000kuba@gmail.com>
2023-04-23 01:00:41 +0300

    ex_10

--- function/function.py
+++ function/function.py
@@ -1,1 +1,12 @@
-
+import random
+list = [random.randint(0,10) for i in range(20)]
+print(list)
+def most_common_number(list):
+    counts = {}
+    for num in list:
+        if num in counts:
+            counts[num] += 1
+        else:
+            counts[num] = 1
+    return max(counts, key=lambda x: (counts[x], -x))
+print(most_common_number(list))
