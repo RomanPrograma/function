f261d98afc401736b3b3ea1cd35f6265fe301921
Roman <roma2000kuba@gmail.com>
2023-04-23 00:58:14 +0300

    ex_2 done

--- function/function.py
+++ function/function.py
@@ -1,1 +1,18 @@
 
+def count_volves(string):
+    volves = 'aeiouyAEIOUY'
+    count_volves = 0
+    for letter in string:
+        if letter in volves:
+            count_volves += 1
+    return count_volves 
+def count_consonants(string):
+    consonants = 'abcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZ'
+    count_consonants = 0
+    for letter in string:
+        if letter in consonants:
+            count_consonants += 1   
+    return count_consonants
+string = str(input('enter text'))
+print('count_volves: ',count_volves(string))
+print('count_consonants: ',count_consonants(string))
